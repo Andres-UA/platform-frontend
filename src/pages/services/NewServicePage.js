64c5ad0b960a1e8678d3f16a7c11d7e6595c7f24
Andrés U <andrewu121@gmail.com>
2019-10-10 15:45:57 -0500

    add service page

--- src/pages/services/NewServicePage.js
+++ src/pages/services/NewServicePage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import API from '../../api';
-import { Row, Col, Button, Card, Form } from 'react-bootstrap';
+import { Row, Col, Button, Card, Form, Modal, Container } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 
 import Aux from '../../hoc/_Aux';
@@ -11,75 +11,176 @@ class NewServicePage extends Component {
     this.state = {
       name: '',
       description: '',
-      inputsValues: [{ name: '', type: 'Numero', isRequired: false, default: '' }]
+      inputs: [{ name: '', type: 'Numero', isRequired: false, default: '' }],
+      assets: [],
+      participants: [],
+      modal: false,
+      modalType: '',
+      tableName: '',
     };
-    this.onAddItem = this.onAddItem.bind(this);
-    this.handleInputNameChange = this.handleInputNameChange.bind(this);
-    this.handleNameChange = this.handleNameChange.bind(this);
-    this.handleDescriptionChange = this.handleDescriptionChange.bind(this);
-    this.handleInputTypeChange = this.handleInputTypeChange.bind(this);
-    this.handleInputRequiredChange = this.handleInputRequiredChange.bind(this);
-    this.handleInputDefaultChange = this.handleInputDefaultChange.bind(this);
+    this.handleServiceNameChange = this.handleServiceNameChange.bind(this);
+    this.handleServiceDescriptionChange = this.handleServiceDescriptionChange.bind(this);
+    this.handleInputsNameChange = this.handleInputsNameChange.bind(this);
+    this.handleInputsTypeChange = this.handleInputsTypeChange.bind(this);
+    this.handleInputsRequiredChange = this.handleInputsRequiredChange.bind(this);
+    this.handleInputsDefaultChange = this.handleInputsDefaultChange.bind(this);
+
+    this.handleTableNameChange = this.handleTableNameChange.bind(this);
+
+    this.toggleModal = this.toggleModal.bind(this);
+    this.toggleParticipantModal = this.toggleParticipantModal.bind(this);
+    this.toggleAssetModal = this.toggleAssetModal.bind(this);
+
+    this.onAddInput = this.onAddInput.bind(this);
+    this.onRemoveInput = this.onRemoveInput.bind(this);
+
+    this.onAddModel = this.onAddModel.bind(this);
+
     this.onSubmit = this.onSubmit.bind(this);
   }
 
   static contextTypes = {
-    router: PropTypes.object
+    router: PropTypes.object,
   };
 
-  handleNameChange(event) {
+  toggleParticipantModal() {
+    this.setState({
+      modalType: 'participant',
+      modal: !this.state.modal,
+    });
+  }
+
+  toggleAssetModal() {
+    this.setState({
+      modalType: 'asset',
+      modal: !this.state.modal,
+    });
+  }
+
+  toggleModal() {
+    this.setState({
+      modalType: '',
+      modal: !this.state.modal,
+    });
+  }
+
+  handleServiceNameChange(event) {
     this.setState({ name: event.target.value });
   }
 
-  handleDescriptionChange(event) {
+  handleServiceDescriptionChange(event) {
     this.setState({ description: event.target.value });
   }
 
-  handleInputNameChange = index => event => {
-    const newInputsValues = this.state.inputsValues.map((input, idx) => {
+  handleTableNameChange(event) {
+    this.setState({ tableName: event.target.value });
+  }
+
+  handleInputsNameChange = index => event => {
+    const inputs = this.state.inputs.map((input, idx) => {
       if (index !== idx) return input;
       return { ...input, name: event.target.value };
     });
-    this.setState({ inputsValues: newInputsValues });
+    this.setState({ inputs });
   };
 
-  handleInputTypeChange = index => event => {
-    const newInputsValues = this.state.inputsValues.map((input, idx) => {
+  handleInputsTypeChange = index => event => {
+    const inputs = this.state.inputs.map((input, idx) => {
       if (index !== idx) return input;
       return { ...input, type: event.target.value };
     });
-    this.setState({ inputsValues: newInputsValues });
+    this.setState({ inputs });
   };
 
-  handleInputRequiredChange = index => event => {
-    const newInputsValues = this.state.inputsValues.map((input, idx) => {
+  handleInputsRequiredChange = index => event => {
+    const inputs = this.state.inputs.map((input, idx) => {
       if (index !== idx) return input;
       return { ...input, isRequired: event.target.checked };
     });
-    this.setState({ inputsValues: newInputsValues });
+    this.setState({ inputs });
   };
 
-  handleInputDefaultChange = index => event => {
-    const newInputsValues = this.state.inputsValues.map((input, idx) => {
+  handleInputsDefaultChange = index => event => {
+    const inputs = this.state.inputs.map((input, idx) => {
       if (index !== idx) return input;
       return { ...input, default: event.target.value };
     });
-    this.setState({ inputsValues: newInputsValues });
+    this.setState({ inputs });
   };
 
-  onAddItem() {
+  onRemoveInput = index => event => {
+    const inputs = this.state.inputs.filter((input, idx) => {
+      return index !== idx;
+    });
+    this.setState({ inputs });
+  };
+
+  onAddInput() {
     this.setState(state => {
-      const inputsValues = state.inputsValues.concat({ name: '', type: 'Numero', isRequired: false, default: '' });
+      const inputs = state.inputs.concat({
+        name: '',
+        type: 'Numero',
+        isRequired: false,
+        default: '',
+      });
       return {
-        inputsValues
+        inputs,
       };
+    });
+  }
+
+  onAddModel() {
+    this.setState(state => {
+      if (state.modalType === 'participant') {
+        const newData = state.participants.concat({
+          name: state.tableName,
+          data: state.inputs,
+        });
+        return {
+          participants: newData,
+          inputs: [
+            {
+              name: '',
+              type: 'Numero',
+              isRequired: false,
+              default: '',
+            },
+          ],
+          tableName: '',
+          modal: false,
+          modalType: '',
+        };
+      } else {
+        const newData = state.assets.concat({
+          name: state.tableName,
+          data: state.inputs,
+        });
+        return {
+          assets: newData,
+          inputs: [
+            {
+              name: '',
+              type: 'Numero',
+              isRequired: false,
+              default: '',
+            },
+          ],
+          tableName: '',
+          modal: false,
+          modalType: '',
+        };
+      }
     });
   }
 
   onSubmit(event) {
     event.preventDefault();
 
-    API.post(`service/`, { name: this.state.name, description: this.state.description, model_schema: this.state.inputsValues })
+    API.post(`service/`, {
+      name: this.state.name,
+      description: this.state.description,
+      model_schema: this.state.inputsValues,
+    })
       .then(res => {
         const _id = res.data.id;
         this.context.router.history.push(`/dashboard/services/${_id}`);
@@ -90,7 +191,7 @@ class NewServicePage extends Component {
   }
 
   render() {
-    const inputs = this.state.inputsValues.map((input, index) => {
+    const inputs = this.state.inputs.map((participant, index) => {
       return (
         <Row key={index}>
           <Col md={3}>
@@ -100,8 +201,8 @@ class NewServicePage extends Component {
                 name={`name-${index}`}
                 id={`name-${index}`}
                 data-context="input"
-                value={input.name}
-                onChange={this.handleInputNameChange(index)}
+                value={participant.name}
+                onChange={this.handleInputsNameChange(index)}
               />
             </Form.Group>
           </Col>
@@ -114,8 +215,8 @@ class NewServicePage extends Component {
                 name={`type-${index}`}
                 id={`type-${index}`}
                 data-context="input"
-                value={input.type}
-                onChange={this.handleInputTypeChange(index)}
+                value={participant.type}
+                onChange={this.handleInputsTypeChange(index)}
               >
                 <option>Numero</option>
                 <option>Cadena</option>
@@ -124,7 +225,7 @@ class NewServicePage extends Component {
             </Form.Group>
           </Col>
 
-          <Col md={3}>
+          <Col md={2}>
             <center>
               <Form.Group>
                 <Form.Check
@@ -132,8 +233,8 @@ class NewServicePage extends Component {
                   name={`required-${index}`}
                   id={`required-${index}`}
                   data-context="input"
-                  checked={input.isRequired}
-                  onChange={this.handleInputRequiredChange(index)}
+                  checked={participant.isRequired}
+                  onChange={this.handleInputsRequiredChange(index)}
                 />
               </Form.Group>
             </center>
@@ -146,12 +247,51 @@ class NewServicePage extends Component {
                 name={`default-${index}`}
                 id={`default-${index}`}
                 data-context="input"
-                value={input.default}
-                onChange={this.handleInputDefaultChange(index)}
+                value={participant.default}
+                onChange={this.handleInputsDefaultChange(index)}
               />
             </Form.Group>
           </Col>
+          <Col md={1}>
+            <Button variant="danger" onClick={this.onRemoveInput(index)}>
+              <b>-</b>
+            </Button>
+          </Col>
         </Row>
+      );
+    });
+
+    const participants = this.state.participants.map((participant, index) => {
+      return (
+        <Col xs={3} key={index}>
+          <Card>
+            <Card.Header>
+              <Card.Title as="h5">{participant.name}</Card.Title>
+            </Card.Header>
+            <Card.Body>
+              <ul>
+                <li>Campo 1</li>
+              </ul>
+            </Card.Body>
+          </Card>
+        </Col>
+      );
+    });
+
+    const assets = this.state.assets.map((asset, index) => {
+      return (
+        <Col xs={3} key={index}>
+          <Card>
+            <Card.Header>
+              <Card.Title as="h5">{asset.name}</Card.Title>
+            </Card.Header>
+            <Card.Body>
+              <ul>
+                <li>Campo 1</li>
+              </ul>
+            </Card.Body>
+          </Card>
+        </Col>
       );
     });
 
@@ -159,63 +299,76 @@ class NewServicePage extends Component {
       <Aux>
         <Row>
           <Col>
+            <h3>Nuevo Servicio</h3>
             <Card>
               <Card.Header>
-                <Card.Title as="h5">Nuevo servicio</Card.Title>
+                <Card.Title as="h5">Datos Basicos</Card.Title>
               </Card.Header>
               <Card.Body>
                 <Form>
                   <Form.Group>
                     <Form.Label>Nombre del servicio</Form.Label>
-                    <Form.Control type="text" value={this.state.name} onChange={this.handleNameChange} />
+                    <Form.Control
+                      type="text"
+                      value={this.state.name}
+                      onChange={this.handleServiceNameChange}
+                    />
                   </Form.Group>
 
                   <Form.Group>
                     <Form.Label>Descripcion del servicio</Form.Label>
-                    <Form.Control as="textarea" rows="3" value={this.state.description} onChange={this.handleDescriptionChange} />
+                    <Form.Control
+                      as="textarea"
+                      rows="3"
+                      value={this.state.description}
+                      onChange={this.handleServiceDescriptionChange}
+                    />
                   </Form.Group>
-
-                  <p>Modelo de datos</p>
-
-                  <Row>
-                    <Col md={3}>
-                      <center>
-                        <Form.Group>
-                          <Form.Label>Nombre del campo</Form.Label>
-                        </Form.Group>
-                      </center>
-                    </Col>
-                    <Col md={3}>
-                      <center>
-                        <Form.Group>
-                          <Form.Label>Tipo de dato del campo</Form.Label>
-                        </Form.Group>
-                      </center>
-                    </Col>
-
-                    <Col md={3}>
-                      <center>
-                        <Form.Group>
-                          <Form.Label>Es requerido?</Form.Label>
-                        </Form.Group>
-                      </center>
-                    </Col>
-
-                    <Col md={3}>
-                      <center>
-                        <Form.Group>
-                          <Form.Label>Valor por defecto</Form.Label>
-                        </Form.Group>
-                      </center>
-                    </Col>
-                  </Row>
-                  {inputs}
-
-                  <Button variant="success" onClick={this.onAddItem}>
-                    Nuevo campo
-                  </Button>
                 </Form>
               </Card.Body>
+            </Card>
+            <Card>
+              <Card.Header>
+                <Card.Title as="h5">Modelo de Datos</Card.Title>
+              </Card.Header>
+              <Card.Body>Participantes, Activos...</Card.Body>
+            </Card>
+            <h3>Participantes</h3>
+            <Row>
+              {participants}
+              <Col xs={3}>
+                <Card>
+                  <Card.Body>
+                    <center>
+                      <Button variant="success" onClick={this.toggleParticipantModal}>
+                        Nuevo Participante
+                      </Button>
+                    </center>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
+            <h3>Activos</h3>
+            <Row>
+              {assets}
+              <Col xs={3}>
+                <Card>
+                  <Card.Body>
+                    <center>
+                      <Button variant="success" onClick={this.toggleAssetModal}>
+                        Nuevo Activo
+                      </Button>
+                    </center>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
+
+            <Card>
+              <Card.Header>
+                <Card.Title as="h5">Para finalizar</Card.Title>
+              </Card.Header>
+              <Card.Body>Revisa todos tus datos... Ten en cuenta que ...</Card.Body>
               <Card.Footer>
                 <center>
                   <Button variant="primary" onClick={this.onSubmit}>
@@ -226,7 +379,80 @@ class NewServicePage extends Component {
             </Card>
           </Col>
         </Row>
-        <Row></Row>
+
+        <Modal size="lg" show={this.state.modal} onHide={this.toggleModal}>
+          <Modal.Header closeButton>
+            <Modal.Title>
+              Nuevo {this.state.modalType === 'participant' ? 'Participante' : 'Activo'}
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Container>
+              <Row>
+                <Col md={12}>
+                  <Form.Group>
+                    <Form.Label>
+                      Nombre del{' '}
+                      {this.state.modalType === 'participant' ? 'Participante' : 'Activo'}
+                    </Form.Label>
+                    <Form.Control
+                      type="text"
+                      value={this.state.tableName}
+                      onChange={this.handleTableNameChange}
+                    />
+                  </Form.Group>
+                </Col>
+                <Col md={12}>
+                  <h5>Atributos</h5>
+                </Col>
+                <Col md={3}>
+                  <center>
+                    <Form.Group>
+                      <Form.Label>Nombre del campo</Form.Label>
+                    </Form.Group>
+                  </center>
+                </Col>
+                <Col md={3}>
+                  <center>
+                    <Form.Group>
+                      <Form.Label>Tipo de dato del campo</Form.Label>
+                    </Form.Group>
+                  </center>
+                </Col>
+
+                <Col md={2}>
+                  <center>
+                    <Form.Group>
+                      <Form.Label>Es requerido?</Form.Label>
+                    </Form.Group>
+                  </center>
+                </Col>
+
+                <Col md={3}>
+                  <center>
+                    <Form.Group>
+                      <Form.Label>Valor por defecto</Form.Label>
+                    </Form.Group>
+                  </center>
+                </Col>
+              </Row>
+              {inputs}
+            </Container>
+            <center>
+              <Button variant="success" onClick={this.onAddInput}>
+                Nuevo Campo
+              </Button>
+            </center>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={this.toggleModal}>
+              Cancelar
+            </Button>
+            <Button variant="primary" onClick={this.onAddModel}>
+              Guardar
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </Aux>
     );
   }
